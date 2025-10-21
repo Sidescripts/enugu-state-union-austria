d495807e619f7114587a8cacf04adf902fbb3b14
Sidescripts <dubyernest14@gmail.com>
2025-10-21 21:41:19 +0100

    prod-check-2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,15 +8,15 @@ module.exports = {
     ignore_watch: ['node_modules', 'logs'],
     max_memory_restart: '512M', // Lowered for shared hosting
     autorestart: true,
-    out_file: '/home/qyybymqv/gratoms/server/logs/gratom-app-out.log',
-    error_file: '/home/qyybymqv/gratoms/server/logs/gratom-app-error.log',
+    out_file: '/home/pzclywaz/esua/logs/esua-app-out.log',
+    error_file: '/home/pzclywaz/esua/logs/esua-app-error.log',
     log_date_format: 'YYYY-MM-DD HH:mm:ss',
     max_size: '10M', // Rotate logs at 10MB
     retain: 7, // Keep 7 days of logs
     cron_restart: '0 1 * * *', // Restart after ROI cron
     env: {
       NODE_ENV: 'production',
-      DOTENV_CONFIG_PATH: '/home/qyybymqv/gratoms/.env'
+      DOTENV_CONFIG_PATH: '/home/pzclywaz/esua.env'
     },
     env_development: {
       NODE_ENV: 'development',
